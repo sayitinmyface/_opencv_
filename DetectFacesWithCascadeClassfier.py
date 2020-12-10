8a6e7876da92a1f47817f7aa2f9cf3608c064d55
sayitinmyface <leo99roo@gmail.com>
2020-12-11 08:20:59 +0900

    1211

---
+++ DetectFacesWithCascadeClassfier.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+def main():
+    cascade = cv.CascadeClassifier('datas/haar_cascade_files/haarcascade_frontalface_default.xml')
+    img = cv.imread('datas/images/faces.jpg')
+    imgG = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    faces = cascade.detectMultiScale(imgG,1.1,4)
+    for x,y,w,h in faces:
+        cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    cv.imshow("R",img)        
+    cv.waitKey(0)
+    
+if __name__ == "__main__":
+    main()    
